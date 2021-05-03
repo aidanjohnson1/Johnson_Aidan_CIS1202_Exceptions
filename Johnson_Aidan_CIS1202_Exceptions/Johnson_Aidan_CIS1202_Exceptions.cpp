512203869d6d129cc3412d3668a6edc453be4b12
Aidan Johnson <aidtomjohn624@gmail.com>
2021-05-02 22:08:13 -0400

    Edited character function so that the start character must be the same case as the target character; otherwise, and exception is thrown. Also added main driver to take input from user and return the value of the target character, as long as no exceptions are thrown.

--- Johnson_Aidan_CIS1202_Exceptions/Johnson_Aidan_CIS1202_Exceptions.cpp
+++ Johnson_Aidan_CIS1202_Exceptions/Johnson_Aidan_CIS1202_Exceptions.cpp
@@ -12,7 +12,21 @@ char character(char, int);
 
 int main()
 {
+	//Declare variables
+	char startChar;
+	int offsetInt;
 
+	//Obtain values from user
+	cout << "What character is your starting position?\n";
+	cin >> startChar;
+
+	cout << "How much do you want to offset this character?\n";
+	cin >> offsetInt;
+
+	//Call in function to either throw exceptions or validate, and then return the offset character
+	cout << character(startChar, offsetInt);
+
+	return 0;
 }
 
 char character(char start, int offset)
@@ -24,6 +38,10 @@ char character(char start, int offset)
 
 		try {
 			if (!isalpha(start + offset)) { //If target letter is "below" the letter a, throw offset exception; otherwise, go on
+				throw offset;
+			}
+
+			if (isupper(start) != isupper(start + offset)) { //Ensure that the start and target characters are of the same case; otherwise, return offset exception
 				throw offset;
 			}
 
