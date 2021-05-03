41f36463219dd590729c0f0a74d92bd6219e1f1a
Aidan Johnson <aidtomjohn624@gmail.com>
2021-05-02 21:57:11 -0400

    Added character function that accepted a character variable and integer variable. The function then tests if the character is a letter, and the target letter exists, then return the target letter. If either of these values are invalid, then throw an exception. After printing the exception, return null to end the program.

--- Johnson_Aidan_CIS1202_Exceptions/Johnson_Aidan_CIS1202_Exceptions.cpp
+++ Johnson_Aidan_CIS1202_Exceptions/Johnson_Aidan_CIS1202_Exceptions.cpp
@@ -2,12 +2,39 @@
 //CIS 1201 101
 //May 2, 2021
 #include <iostream>
+#include <string>
+#include <ctype.h>
+#include <exception>
 using namespace std;
+
+//Prototypes
+char character(char, int);
 
 int main()
 {
 
+}
 
+char character(char start, int offset)
+{
+	try {
+		if (!isalpha(start)) { //If character is a letter, go on; otherwise, throw start exception
+			throw start;
+		}
 
+		try {
+			if (!isalpha(start + offset)) { //If target letter is "below" the letter a, throw offset exception; otherwise, go on
+				throw offset;
+			}
 
+			return start + offset; //Return the target letter
+		}
+		catch (int targetException) {
+			cout << "invalidRangeException, Offset: " << targetException << " is not within the range.\n";
+			return NULL;
+		}
+	} catch (char letterException) {
+		cout << "invalidCharacterException, Character: " << letterException << " is not a valid letter.\n";
+		return NULL;
+	}
 }
